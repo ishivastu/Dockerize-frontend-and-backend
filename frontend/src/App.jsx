8ed7bf8700a9434b2e5b5fa9d4f6e8e0903c63bc
Shivastu Mishra <shivastumishra@gmail.com>
2026-09-17 17:51:59 +0530

    v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8000/api/data', { name, email, message });
+      const response = await axios.post('https://dockerize-frontend-and-backend.onrender.com/api/data', { name, email, message });
       alert('Message sent successfully!');
       console.log('Response:', response);
       setName('');
